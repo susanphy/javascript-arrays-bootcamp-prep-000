46f6d521a85b663fe7b40ab20382e5be07597b9e
Susan <suzutoyuki@gmail.com>
2018-01-23 02:21:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
  return array.slice(1);
  }
-function destructivelyRemoveElementFromBeginningOfArray (array){
+function destructivelyRemoveElementFromEndOfArray (array){
   return array.pop();
 }
