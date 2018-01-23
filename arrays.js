3571e38139968fb430757dd5d31fb9ef8724cc1c
Susan <suzutoyuki@gmail.com>
2018-01-23 02:27:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,3 +34,8 @@ function destructivelyRemoveElementFromEndOfArray (array){
   array.pop();
   return array;
 }
+function removeElementFromEndOfArray (array){
+  array.slice(0,array.length -1);
+  return array;
+}
+}]
