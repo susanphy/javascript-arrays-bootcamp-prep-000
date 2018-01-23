aa225f047d04e9cc81d0ec3a30a10d1f22fc500c
Susan <suzutoyuki@gmail.com>
2018-01-23 01:36:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,6 @@ function destructivelyAddElementToEndOfArray (array,element){
 function accessElementInArray (array, index){
   return array[index];
 }
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  return array.slice(1);
+}
